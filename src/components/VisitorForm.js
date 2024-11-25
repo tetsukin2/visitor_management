7e9eb2abe4f1a74155235c8a9d7e70a1f70fb049
sasing <vencesasing13@gmail.com>
2024-11-25 14:46:27 +0800

    added translation error fix

--- src/components/VisitorForm.js
+++ src/components/VisitorForm.js
@@ -1,5 +1,7 @@
 "use client";
 
+import Head from "next/head";
+
 import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { DateTime } from "luxon";
@@ -24,23 +26,32 @@ export default function VisitorForm() {
   const [showConfirmation, setShowConfirmation] = useState(false);
   const [language, setLanguage] = useState("zh");
   const [showOtherField, setShowOtherField] = useState(false);
+  const [branch, setBranch] = useState(null);
 
   const t = translations[language] || {};
   const sharedT = sharedTranslations[language] || {};
 
   useEffect(() => {
     try {
-      const savedFormData = localStorage.getItem("visitorFormData");
-      const isCheckedOut = localStorage.getItem("checkedOut");
+      document.documentElement.setAttribute("translate", "no");
+      document.documentElement.lang = "en";
 
-      if (savedFormData && isCheckedOut !== "true") {
-        const parsedData = JSON.parse(savedFormData);
-        console.log("Restored visitor data from localStorage:", parsedData);
-        setFormData(parsedData);
-        setSubmitted(true);
-      } else {
-        localStorage.removeItem("visitorFormData");
-        localStorage.removeItem("checkedOut");
+      if (typeof window !== "undefined") {
+        const savedFormData = localStorage.getItem("visitorFormData");
+        const isCheckedOut = localStorage.getItem("checkedOut");
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const branchParam = urlParams.get("branch") || "branch1";
+        setBranch(branchParam);
+
+        if (savedFormData && isCheckedOut !== "true") {
+          const parsedData = JSON.parse(savedFormData);
+          setFormData(parsedData);
+          setSubmitted(true);
+        } else {
+          localStorage.removeItem("visitorFormData");
+          localStorage.removeItem("checkedOut");
+        }
       }
     } catch (error) {
       console.error("Error reading localStorage data:", error);
@@ -53,7 +64,6 @@ export default function VisitorForm() {
 
   const handleReasonChange = (e) => {
     const value = e.target.value.trim();
-
     setFormData({ ...formData, visitReason: value });
 
     if (value.toLowerCase() === "other (specify)" || value.includes("其它")) {
@@ -69,14 +79,10 @@ export default function VisitorForm() {
     const formID = uuidv4();
     const visitTime = DateTime.now().setZone("Asia/Taipei").toISO();
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const branch = urlParams.get("branch") || "branch1";
-
     const finalFormData = { ...formData, formID, visitTime, branch };
 
     try {
       await checkIn(finalFormData);
-      console.log("Check-in data:", finalFormData);
       setFormData(finalFormData);
       localStorage.setItem("visitorFormData", JSON.stringify(finalFormData));
       setSubmitted(true);
@@ -93,8 +99,6 @@ export default function VisitorForm() {
 
     try {
       await checkOut(checkoutData);
-      console.log("Check-out data:", checkoutData);
-
       localStorage.removeItem("visitorFormData");
       localStorage.setItem("checkedOut", "true");
       setCheckedOut(true);
@@ -144,92 +148,98 @@ export default function VisitorForm() {
         onSignOut={handleEndVisit}
         sharedT={sharedT}
         formData={formData}
+        branch={branch} // Pass the branch to SubmittedScreen
         navigateToConfirm={navigateToConfirm}
       />
     );
   }
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-gray-100 p-4 w-11/12">
-      <Header
-        language={language}
-        toggleLanguage={() => setLanguage(language === "en" ? "zh" : "en")}
-        title={sharedT.visitorRegistration}
-      />
+    <>
+      <Head>
+        <meta name="google" content="notranslate" />
+      </Head>
+      <div className="h-screen flex flex-col items-center justify-center bg-gray-100 p-4 w-11/12">
+        <Header
+          language={language}
+          toggleLanguage={() => setLanguage(language === "en" ? "zh" : "en")}
+          title={sharedT.visitorRegistration}
+        />
 
-      <WhiteContainer>
-        <form onSubmit={handleCheckIn} className="space-y-4">
-          <FormField
-            label={t.form?.visitorName || "Visitor's Name"}
-            name="visitorName"
-            required
-            onChange={handleInputChange}
-          />
-          <FormField
-            label={t.form?.visitorCompany || "Visitor's Company"}
-            name="visitorCompany"
-            required
-            onChange={handleInputChange}
-          />
-          <FormField
-            label={t.form?.hostName || "Host's Name"}
-            name="hostName"
-            required
-            onChange={handleInputChange}
-          />
-          <FormField
-            label={t.form?.hostDepartment || "Host's Department"}
-            name="hostDepartment"
-            required
-            onChange={handleInputChange}
-          />
-          <FormField
-            label={t.form?.hostPosition || "Host's Position"}
-            name="hostPosition"
-            required
-            onChange={handleInputChange}
-          />
-          <FormField
-            label={t.form?.visitReason || "Reason for Visit"}
-            name="visitReason"
-            required
-            onChange={handleReasonChange}
-            options={t.form?.visitReasonOptions || []}
-          />
-          {showOtherField && (
+        <WhiteContainer>
+          <form onSubmit={handleCheckIn} className="space-y-4">
             <FormField
-              label={sharedT.otherSpecify}
-              name="visitReasonDetail"
-              required={showOtherField}
+              label={t.form?.visitorName || "Visitor's Name"}
+              name="visitorName"
+              required
               onChange={handleInputChange}
             />
-          )}
-          <FormField
-            label={t.form?.temperature || "Temperature"}
-            name="temperature"
-            required
-            onChange={handleInputChange}
-          />
-          <FormField
-            label={t.form?.specialRequirements || "Special Requirements"}
-            name="specialRequirements"
-            isTextarea
-            onChange={handleInputChange}
-          />
-          <FormField
-            label={t.form?.irregularities || "Irregularities"}
-            name="irregularities"
-            isTextarea
-            onChange={handleInputChange}
-          />
-          <button
-            type="submit"
-            className="bg-blue-600 text-white py-2 px-4 rounded w-full"
-          >
-            {t.buttons?.submit || "Submit"}
-          </button>
-        </form>
-      </WhiteContainer>
-    </div>
+            <FormField
+              label={t.form?.visitorCompany || "Visitor's Company"}
+              name="visitorCompany"
+              required
+              onChange={handleInputChange}
+            />
+            <FormField
+              label={t.form?.hostName || "Host's Name"}
+              name="hostName"
+              required
+              onChange={handleInputChange}
+            />
+            <FormField
+              label={t.form?.hostDepartment || "Host's Department"}
+              name="hostDepartment"
+              required
+              onChange={handleInputChange}
+            />
+            <FormField
+              label={t.form?.hostPosition || "Host's Position"}
+              name="hostPosition"
+              required
+              onChange={handleInputChange}
+            />
+            <FormField
+              label={t.form?.visitReason || "Reason for Visit"}
+              name="visitReason"
+              required
+              onChange={handleReasonChange}
+              options={t.form?.visitReasonOptions || []}
+            />
+            {showOtherField && (
+              <FormField
+                label={sharedT.otherSpecify}
+                name="visitReasonDetail"
+                required={showOtherField}
+                onChange={handleInputChange}
+              />
+            )}
+            <FormField
+              label={t.form?.temperature || "Temperature"}
+              name="temperature"
+              required
+              onChange={handleInputChange}
+            />
+            <FormField
+              label={t.form?.specialRequirements || "Special Requirements"}
+              name="specialRequirements"
+              isTextarea
+              onChange={handleInputChange}
+            />
+            <FormField
+              label={t.form?.irregularities || "Irregularities"}
+              name="irregularities"
+              isTextarea
+              onChange={handleInputChange}
+            />
+            <button
+              type="submit"
+              className="bg-blue-600 text-white py-2 px-4 rounded w-full"
+            >
+              {t.buttons?.submit || "Submit"}
+            </button>
+          </form>
+        </WhiteContainer>
+      </div>
+    </>
   );
 }
